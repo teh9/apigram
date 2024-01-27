13ae829d1212fea86de89a718d44ef6e92136c4a
teh9 <teh9@inbox.lv>
2024-01-27 13:15:04 +0200

    Replaced from env vars to method

--- tests/MessagesTest.php
+++ tests/MessagesTest.php
@@ -11,21 +11,12 @@ class MessagesTest extends TestCase
 
     protected $chatId;
 
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->accessToken = getenv('TELEGRAM_BOT_TOKEN');
-        $this->chatId = getenv('TELEGRAM_CHAT_ID');
-    }
-
     public function testSendMessage()
     {
-        $this->assertIsString($this->accessToken);
-        $this->assertIsString($this->chatId);
-        // $telegram = new TelegramClient($this->accessToken);
-        // $response = $telegram->messages()->to($this->chatId)->send('test');
+        $telegram = new TelegramClient(getenv('TELEGRAM_BOT_TOKEN'));
+        $response = $telegram->messages()->to(getenv('TELEGRAM_CHAT_ID'))->send('test');
 
-        // $this->assertTrue($response->status());
-        // $this->assertIsInt($response->getMessageId());
+        $this->assertTrue($response->status());
+        $this->assertIsInt($response->getMessageId());
     }
 }
