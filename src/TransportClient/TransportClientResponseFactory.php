1822860a150d17698823b21e4e10c297eec04e34
teh9 <teh9@inbox.lv>
2024-01-27 14:25:54 +0200

    Fix for factory

--- src/TransportClient/TransportClientResponseFactory.php
+++ src/TransportClient/TransportClientResponseFactory.php
@@ -7,7 +7,7 @@ use Teh9\Apigram\TransportClient\Responses\MessagesResponse;
 
 class TransportClientResponseFactory
 {
-    public static function make(string $action)
+    public static function make(?string $action)
     {
         switch ($action) {
             case 'messages': return new MessagesResponse;
