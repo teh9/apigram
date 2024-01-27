e2a652cc801afe8724f5e250cd87144df2b457c3
teh9 <teh9@inbox.lv>
2024-01-27 11:05:30 +0200

    Added default response factory

--- src/TransportClient/TransportClientResponseFactory.php
+++ src/TransportClient/TransportClientResponseFactory.php
@@ -2,12 +2,15 @@
 
 namespace Teh9\Apigram\TransportClient\Responses;
 
+use Teh9\Apigram\TransportClient\TransportClientResponse;
+
 class TransportClientResponseFactory
 {
     public static function make(string $action)
     {
         switch ($action) {
             case 'messages': return new MessagesResponse;
+            default: return new TransportClientResponse;
         }
     }
 }
