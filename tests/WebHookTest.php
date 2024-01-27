d9697d5138dbd1ca67af2af950bc8304eca42fca
teh9 <teh9@inbox.lv>
2024-01-27 14:44:22 +0200

    Fixed test

--- tests/WebHookTest.php
+++ tests/WebHookTest.php
@@ -10,15 +10,20 @@ class WebHookTest extends TestCase
     public function testSetWrongWebhook()
     {
         $telegram = new TelegramClient(getenv('TELEGRAM_BOT_TOKEN'));
-        $response = $telegram->webhook()->set('http://localhost');
-
-        $this->assertFalse($response->status());
+        try {
+            $response = $telegram->webhook()->set('http://localhost');
+            $this->fail('Expected exception not thrown');
+        } catch (\Exception $e) {
+            $this->assertFalse($response->status());
+            $this->assertEquals(400, $response->getCode());
+        }
     }
 
     public function testSetWebhook()
     {
+        sleep(1);
         $telegram = new TelegramClient(getenv('TELEGRAM_BOT_TOKEN'));
-        $response = $telegram->webhook()->set('https://github.com');
+        $response = $telegram->webhook()->set(getenv('WEBHOOK_URL'));
 
         $this->assertTrue($response->status());
     }
