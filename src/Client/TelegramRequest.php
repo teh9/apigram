543c52f400e05bce1489db1d54a1f66deab9e73a
teh9 <teh9@inbox.lv>
2024-02-12 11:39:10 +0200

    var_dump

--- src/Client/TelegramRequest.php
+++ src/Client/TelegramRequest.php
@@ -44,6 +44,7 @@ class TelegramRequest
     public function post(string $method, array $params = [])
     {
         $params = $this->prepareRequest($params);
+        echo '<pre>'; print_r($params); echo '</pre>';
         
         return $this->request($method, ['form_params' => $params]);
     }
@@ -51,7 +52,8 @@ class TelegramRequest
     public function multipart(string $method, array $params = [])
     {
         $params = $this->prepareRequest($params);
-        
+        echo '<pre>'; print_r($params); echo '</pre>';
+
         return $this->request($method, ['multipart' => $params]);
     }
     
